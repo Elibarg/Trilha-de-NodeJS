362bace06cd03a9ca2f823a24333e5863cba735c
Elibarg <calagabriel31@gmail.com>
2025-06-02 21:22:43 -0300

    e

---
+++ index.js
@@ -0,0 +1,19 @@
+let data = new Date();
+
+let hora = data.getHours();
+let min = data.getMinutes();
+
+let saudacao;
+
+if(hora <=11)
+{
+    saudacao = "bom dia";
+}else if(hora <=17)
+{
+    saudacao = "boa tarde";
+}else
+{
+    saudacao = "boa noite";
+}
+console.log('Ola!' + saudacao)
+console.log('Agora são ' + hora + "horas e " + min + "minutos")
