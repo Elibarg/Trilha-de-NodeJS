0d19f18534b1fd1b2b0471d7dd6e26f203427a95
Elibarg <calagabriel31@gmail.com>
2025-06-04 19:37:58 -0300

    fazerOT4

---
+++ OT-NodeJS-04/HTTP/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () =>
+    console.log("Servidor iniciado na porta 3000")
+);
+app.get ('/',(req,res) =>
+res.send('<hi style= "color: blue">CRIANDO UM SERVIDOR EXPRESS.JS</h1>')
+);
