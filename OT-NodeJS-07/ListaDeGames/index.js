508d5ff228472a734c60517fe0b2ca0ad3cdae1d
Elibarg <calagabriel31@gmail.com>
2025-06-17 20:31:29 -0300

    re

---
+++ OT-NodeJS-07/ListaDeGames/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3080, () =>
+    console.log("Servidor iniciado na porta http://localhost:3080")
+);
+
+let games=[
+    {title:"Sea of Thieves",studio:"Rare",price:"30"},
+    {title:"WOW",studio:"Blizzard",price:"120"},
+    {title:"Valorant",studio:"Riot",price:"0"},
+    {title:"COD",studio:"Activision",price:"200"},
+    {title:"Minecreft",studio:"Mojang",price:"80"},
+    {title:"Halo",studio:"Microsoft",price:"90"}
+];
+app.get ('/',(req,res) =>
+    res.json(games)
+ );
+
+ app.use(express.json());
+ app.post("/novogame:index",(req,res)=>
+ {
+    const {index} = req.params;
+    let title = req.body.title;
+    let studio = req.body.studio;
+    let price = req.body.price;
+ 
+    console.log(title);
+    console.log(studio);
+    console.log(price);
+    let newGame ={title,studio,price};
+    games.push(newGame);
+    res.send("OK");
+ });
+
+ app.put("/novogame:index",(req,res)=>
+    {
+       const {index} = req.params;
+       let title = req.body.title;
+       let studio = req.body.studio;
+       let price = req.body.price;
+   
+       games{index} = {title,studio,price};
+       return res.json(games);
+    });
